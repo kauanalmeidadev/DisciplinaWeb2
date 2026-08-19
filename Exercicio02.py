cad9b197c17ec57e84c04506b4917fdab33c422a
Kauan Brito de Almeida <kauanalmeidadev@gmail.com>
2026-08-19 22:14:42 +0000

    Exercicio 2

--- Exercicio02.py
+++ Exercicio02.py
@@ -1,11 +1,11 @@
-numero = int(input("Digite um numero para verificar se é primo:"))
+numero = int(input("Digite um número para verificar se é primo:"))
 
 if numero > 1:
     for i in range(2, int(numero**0.5)+1):
         if (numero % i) == 0:
-            print(numero,"não é um numero primo.")
+            print(numero,"não é um número primo.")
             break
         else:
-            print(numero,"é um numero primo.")
+            print(numero,"é um número primo.")
 else:
-    print(numero,"não é um numero primo.")            
+    print(numero,"não é um número primo.")            
