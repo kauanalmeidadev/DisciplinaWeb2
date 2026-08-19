581335183ed727842b0092cb228776c1be6ff75a
Kauan Brito de Almeida <kauanalmeidadev@gmail.com>
2026-08-19 21:45:40 +0000

    ex001

---
+++ Exercicio1.py
@@ -0,0 +1,12 @@
+numr1 = int (input("Digite o primeiro numero:"))
+numr2 = int (input("Digite o segundo numero:"))
+numr3 = int (input("Digite o terceiro numero"))
+
+resultado_a = (2* numr1) * (numr2 / 2)
+resultado_b = (3* numr1) + numr3
+resultado_c = numr3 ** 3
+
+printf("Porduto do dobro do 1° com metade do 2°:", resultado_a)
+printf("Soma do triplo do 1° com o 3°:", resultado_b)
+printf("Terceiro elevado ao cubo:", resultado_c)
+
