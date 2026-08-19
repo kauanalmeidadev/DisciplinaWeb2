b3ead87f4fa7e89bf18a6fa1d74301ba675144fb
Kauan Brito de Almeida <kauanalmeidadev@gmail.com>
2026-08-19 21:45:57 +0000

    envio

--- Exercicio1.py
+++ Exercicio1.py
@@ -6,7 +6,9 @@ resultado_a = (2* numr1) * (numr2 / 2)
 resultado_b = (3* numr1) + numr3
 resultado_c = numr3 ** 3
 
-printf("Porduto do dobro do 1° com metade do 2°:", resultado_a)
-printf("Soma do triplo do 1° com o 3°:", resultado_b)
-printf("Terceiro elevado ao cubo:", resultado_c)
+print("Porduto do dobro do 1° com metade do 2°:", resultado_a)
+print("Soma do triplo do 1° com o 3°:", resultado_b)
+print("Terceiro elevado ao cubo:", resultado_c)
+
+
 
